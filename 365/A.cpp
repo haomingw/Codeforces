aaaa25e21ec03574130db94f009c2571086070a3
Haoming Wang <haoming.exe@gmail.com>
2016-08-10 12:11:53 +0800

    Code refactor

--- 365/A.cpp
+++ 365/A.cpp
@@ -9,20 +9,20 @@ int main() {
     int mc = 0, cc = 0;
 
     for (int i = 0; i < n; i++) {
-	cin >> m >> c;
-	if (m > c) {
-	    mc++;
-	} else if (m < c) {
-	    cc++;
-	}
+        cin >> m >> c;
+        if (m > c) {
+            mc++;
+        } else if (m < c) {
+            cc++;
+        }
     }
 
     if (mc > cc) {
-	cout << "Mishka" << endl;
+	    cout << "Mishka" << endl;
     } else if (mc < cc) {
-	cout << "Chris" << endl;
+	    cout << "Chris" << endl;
     } else {
-	cout << "Friendship is magic!^^" << endl;
+	    cout << "Friendship is magic!^^" << endl;
     }
 
     return 0;
