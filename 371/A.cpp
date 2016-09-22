dcbaf34a6060335d91a068a8dc1ed2c602116a08
Haoming Wang <haoming.exe@gmail.com>
2016-09-23 00:30:56 +0200

    Meeting of Old Friends

--- 371/A.cpp
+++ 371/A.cpp
@@ -5,22 +5,20 @@ using namespace std;
 typedef long long ll;
 
 int main() {
-	int n, c;
-	cin >> n >> c;
-	int ans = 1;
+	ll l1, r1, l2, r2, k;
+	scanf("%lld %lld %lld %lld %lld", &l1, &r1, &l2, &r2, &k);
+	ll high, low, ans;
+	low = max(l1, l2);
+	high = min(r1, r2);
 
-	if (n == 1) return !printf("1\n");
-	int prev, cur;
-	cin >> prev;
-	for (int i = 1; i < n; i++) {		
-		scanf("%d", &cur);
-		if (cur - prev  <= c) {
-			ans++;			
-		} else {
-			ans = 1;
-		}
-		prev = cur;
+	// clog << low << ", " << high << endl;
+	if (low <= high) {
+		int c = (k >= low && k <= high) ? 1 : 0;
+		ans = high - low + 1 - c;
+	} else {
+		ans = 0;
 	}
+
 	cout << ans << endl;
 	return 0;
 }
