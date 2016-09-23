4b1a4208bd3f0866d0c7e843056c849e2e690364
Haoming Wang <haoming.exe@gmail.com>
2016-09-23 14:57:38 +0200

    Sonya and Queries

---
+++ 371/C.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+typedef long long ll;
+
+ll convert(ll a) {
+	ll r, m = 1, ans = 0;
+	while (a > 0) {
+		r = a % 10;
+		a /= 10;
+		ans += m * (r % 2);
+		m *= 10;
+	}
+	return ans;
+}
+
+int solve(unordered_map<ll, ll>& h, ll s) {
+	int cnt = 0;
+	// clog << "query: ";
+	for (auto& a : h){
+		// clog << a.first << " ";
+		if (a.first == s) {
+			cnt += a.second;
+		}
+	}
+	// clog << " pattern: " << s << endl;
+	return cnt;
+}
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	char c;
+	ll ti, ai;
+	unordered_map<ll, ll> hmap;
+	for (int i = 0; i < n; i++){
+		scanf(" %c %lld", &c, &ti);
+		ai = convert(ti);
+		if (c == '?') {
+			cout << hmap[ai] << endl;
+		} else if (c == '+') {
+			hmap[ai] += 1;
+		} else {
+			hmap[ai] -= 1;
+			if (hmap[ai] == 0) hmap.erase(ai);
+		}
+	}
+	return 0;
+}
