e51f865797cf7c28a61e038afd28959d3b961c7b
Haoming Wang <haoming.exe@gmail.com>
2016-10-20 18:41:37 +0200

    Code template

---
+++ template.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+
+#define FOR(i,a,b) for(int i=(a); i < (b); ++i)
+#define REP(i,a) for(int i = 0; i < (a); ++i)
+#define SIZE(x) ((int)(x).size())
+#define ALL(x) (x).begin(), (x).end()
+
+typedef pair<int, int> pii;
+typedef long long ll;
+
