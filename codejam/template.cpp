3652701cb1c392c4dcc9864fc147f522efba3192
Haoming Wang <haoming.exe@gmail.com>
2017-01-05 01:14:08 +0100

    CodeJam template

---
+++ codejam/template.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <set>
+#include <queue>
+#include <utility>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+using namespace std;
+
+#define REPP(i,a,b) for(int i=(a); i < (b); ++i)
+#define REP(i,a) for(int i = 0; i < (a); ++i)
+#define PER(i,a) for(int i = (a) - 1; i >= 0; --i)
+#define SIZE(x) ((int)(x).size())
+#define ALL(x) (x).begin(), (x).end()
+#define MP make_pair
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+int main() {
+    int T; cin >> T;
+    REP(i, T) {
+        printf("Case #%d: ");
+    }
+    return 0;
+}
