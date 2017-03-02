a774d9d9f53b244a82aa4a30b449431fc2ea779e
Haoming Wang <haoming.exe@gmail.com>
2017-03-02 23:58:50 +0100

    Shell Game

---
+++ 401/A.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <set>
+#include <queue>
+#include <utility>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+using namespace std;
+
+#define REPP(i,a,b) for(int i=(a); i < (b); ++i)
+#define REP(i,a) for(int i = 0; i < (a); ++i)
+#define PER(i,a) for(int i = (a) - 1; i >= 0; --i)
+#define SIZE(x) ((int)(x).size())
+#define ALL(x) (x).begin(), (x).end()
+#define MP make_pair
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+int main() {
+    int n, x;
+    scanf("%d%d", &n, &x);
+    if ((n & 1) && x < 2) x ^= 1;
+    x = (x + n / 2) % 3;
+    cout << x << endl;
+    return 0;
+}
