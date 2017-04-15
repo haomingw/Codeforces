6a3d2b6a1f0f4519e9154f1463ead81a26d398a0
Haoming Wang <haoming.exe@gmail.com>
2017-04-15 10:38:03 +0200

    Alphabet Cake

--- codejam/2017/round1A/A.cpp
+++ codejam/2017/round1A/A.cpp
@@ -24,7 +24,30 @@ typedef long long ll;
 typedef pair<int, int> pii;
 
 void solve() {
-
+    int R, C;
+    cin >> R >> C;
+    string a[R];
+    REP(i, R) cin >> a[i];
+    // first fill horizontal ?s
+    REP(i, R) REP(j, C) {
+        if (a[i][j] != '?') {
+            int p = j + 1;
+            while (p < C && a[i][p] == '?') a[i][p] = a[i][j], p++;
+            p = j - 1;
+            while (p >= 0 && a[i][p] == '?') a[i][p] = a[i][j], p--;
+        }
+    }
+    REP(i, R) REP(j, C) {
+        if (a[i][j] != '?') {
+            int p = i + 1;
+            while (p < R && a[p][j] == '?') a[p][j] = a[i][j], p++;
+            p = i - 1;
+            while (p >= 0 && a[p][j] == '?') a[p][j] = a[i][j], p--;
+        }
+    }
+    REP(i, R) REP(j, C) assert(a[i][j] != '?');
+    cout << endl;
+    REP(i, R) cout << a[i] << endl;
 }
 
 int main() {
