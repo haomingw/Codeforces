bc37268265995a234cd82bd7d3b99e34e499af88
Haoming Wang <haoming.exe@gmail.com>
2017-04-21 23:41:33 +0200

    Vicious Keyboard

--- 409/A.cpp
+++ 409/A.cpp
@@ -23,3 +23,23 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 
+int main() {
+    string s;
+    cin >> s;
+    int n = s.size();
+    int a[105], ans = 0;
+    REP(i, n) {
+        a[i] = s[i] == 'K';
+    }
+    REP(i, n + 1) {
+        a[i] ^= 1;
+        int cnt = 0;
+        REP(j, n - 1) {
+            if (a[j] == 0 && a[j + 1] == 1) cnt++;
+        }
+        ans = max(ans, cnt);
+        a[i] ^= 1;
+    }
+    cout << ans << endl;
+    return 0;
+}
