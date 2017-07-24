0feefb16114d4c100832c1598e896c54099df524
Haoming Wang <haoming.exe@gmail.com>
2017-07-24 10:21:39 +0800

    Sasha and Sticks
    http://codeforces.com/contest/832/problem/A

---
+++ 425/A.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <set>
+#include <queue>
+#include <utility>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <cassert>
+using namespace std;
+
+#define REPP(i,a,b) for(int i=(a); i < (b); ++i)
+#define REP(i,a) for(int i = 0; i < (a); ++i)
+#define PER(i,a) for(int i = (a) - 1; i >= 0; --i)
+#define SIZE(x) ((int)(x).size())
+#define ALL(x) (x).begin(), (x).end()
+#define MP make_pair
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+int main() {
+    ll n, k;
+    cin >> n >> k;
+    ll move = n / k;
+    if (move & 1) cout << "YES" << endl;
+    else cout << "NO" << endl;
+    return 0;
+}
