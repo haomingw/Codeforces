f13007f3df43316e3e074b8d8a6df3a07b7497ee
Haoming Wang <haoming.exe@gmail.com>
2017-08-05 23:12:01 +0800

    Mister B and Book Reading
    http://codeforces.com/contest/820/problem/A

---
+++ 421/A.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <set>
+#include <queue>
+#include <utility>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <cassert>
+using namespace std;
+
+#define REPP(i,a,b) for(int i=(a); i < (b); ++i)
+#define REP(i,a) for(int i = 0; i < (a); ++i)
+#define PER(i,a) for(int i = (a) - 1; i >= 0; --i)
+#define SZ(x) ((int)(x).size())
+#define ALL(x) (x).begin(), (x).end()
+#define MP make_pair
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+int main() {
+    int c, v0, v1, a, l;
+    cin >> c >> v0 >> v1 >> a >> l;
+    int ans = 1, now = v0, t = v0;
+    while (now < c) {
+        t = min(t + a, v1);
+        now += t - l;
+        ans++;
+    }
+    cout << ans << endl;
+    return 0;
+}
