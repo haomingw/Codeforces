358496379b5d53c7a3e8500f5c8572886c26ffce
Haoming Wang <haoming.exe@gmail.com>
2017-08-06 23:43:35 +0800

    Mister B and Angle in Polygon
    http://codeforces.com/contest/820/problem/B

---
+++ 421/B.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <set>
+#include <queue>
+#include <utility>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <cassert>
+using namespace std;
+
+#define REPP(i,a,b) for(int i=(a); i < (b); ++i)
+#define REP(i,a) for(int i = 0; i < (a); ++i)
+#define PER(i,a) for(int i = (a) - 1; i >= 0; --i)
+#define SZ(x) ((int)(x).size())
+#define ALL(x) (x).begin(), (x).end()
+#define MP make_pair
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+int main() {
+    int n, a;
+    cin >> n >> a;
+    double d = a * n / 180.0;
+    int ans = floor(d + 0.5);
+    ans = max(ans, 1);
+    ans = min(ans, n - 2);
+    printf("2 1 %d\n", ans + 2);
+    return 0;
+}
