5920a67e9f185864b0e77ece376524aba8c4bd86
Haoming Wang <haoming.exe@gmail.com>
2017-08-12 22:27:45 +0800

    I'm bored with life
    http://codeforces.com/contest/822/problem/A

---
+++ 422/A.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <set>
+#include <queue>
+#include <utility>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <cassert>
+using namespace std;
+
+#define REPP(i,a,b) for(int i=(a); i < (b); ++i)
+#define REP(i,a) for(int i = 0; i < (a); ++i)
+#define PER(i,a) for(int i = (a) - 1; i >= 0; --i)
+#define SZ(x) ((int)(x).size())
+#define ALL(x) (x).begin(), (x).end()
+#define MP make_pair
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+int main() {
+    int a, b;
+    scanf("%d%d", &a, &b);
+    int c = min(a, b), ans = 1;
+    REP(i, c) ans *= i + 1;
+    printf("%d\n", ans);
+    return 0;
+}
+
