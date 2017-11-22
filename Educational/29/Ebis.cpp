e92a99c0cf65b1dc487d6fee925f448629a7d8b4
Haoming Wang <haoming.exe@gmail.com>
2017-11-22 02:24:45 +0100

    Turn Off The TV (lazy propagation)
    http://codeforces.com/contest/863/problem/E

--- Educational/29/Ebis.cpp
+++ Educational/29/Ebis.cpp
@@ -22,8 +22,32 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 
+const int inf = 1e9;
 const int maxn = 2e5 + 5;
 int l[maxn], r[maxn];
+int rmq[maxn * 8], dd[maxn * 8];
+
+void add(int v, int tl, int tr, int left, int right) {
+    if (tr <= left || tl >= right) return;
+    if (tl >= left && tr <= right) {
+        rmq[v]++;
+        dd[v]++;
+        return;
+    }
+    int m = (tl + tr) >> 1;
+    add(2 * v + 1, tl, m, left, right);
+    add(2 * v + 2, m, tr, left, right);
+    rmq[v] = min(rmq[2 * v + 1], rmq[2 * v + 2]) + dd[v];
+}
+
+int get(int v, int tl, int tr, int left, int right) {
+    if (tr <=left || right <= tl) return inf;
+    if (tl >= left && tr <= right) return rmq[v];
+    int m = (tl + tr) >> 1;
+    int lson = get(2 * v + 1, tl, m, left, right),
+        rson = get(2 * v + 2, m, tr, left, right);
+    return min(lson, rson) + dd[v];
+}
 
 int main() {
     int n;
@@ -38,17 +62,14 @@ int main() {
     sort(ALL(v));
     v.resize(unique(ALL(v)) - v.begin());
     int x = v.size();
-    vector<int> add(x + 1);
     REP(i, n) {
         l[i] = lower_bound(ALL(v), l[i]) - v.begin();
         r[i] = lower_bound(ALL(v), r[i]) - v.begin();
-        add[l[i]]++, add[r[i]]--;
+        add(0, 0, x, l[i], r[i]);
     }
-    REP(i, x) add[i + 1] += add[i];
-    vector<int> sum(x + 1);
-    REP(i, x) sum[i + 1] = sum[i] + (add[i] >= 2);
     REP(i, n) {
-        if (r[i] - l[i] == sum[r[i]] - sum[l[i]]) {
+        int q = get(0, 0, x, l[i], r[i]);
+        if (q >= 2) {
             cout << i + 1 << endl;
             return 0;
         }
