a1f8c12a7096bedd4816d591d569e2f7896f491f
Haoming Wang <haoming.exe@gmail.com>
2018-01-08 00:26:59 +0100

    Modular Exponentiation
    http://codeforces.com/contest/913/problem/A

---
+++ annual/hello2018/A.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <set>
+#include <queue>
+#include <utility>
+#include <algorithm>
+#include <map>
+#include <cassert>
+using namespace std;
+
+#define REPP(i,a,b) for(int i=(a); i < (b); ++i)
+#define REP(i,a) for(int i = 0; i < (a); ++i)
+#define PER(i,a) for(int i = (a) - 1; i >= 0; --i)
+#define SZ(x) ((int)(x).size())
+#define ALL(x) (x).begin(), (x).end()
+#define MP make_pair
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    if (n >= 30) cout << m << endl;
+    else cout << (m % (1 << n)) << endl;
+    return 0;
+}
