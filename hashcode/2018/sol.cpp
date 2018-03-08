4e116a253c3e75f4a64bfd5e8886b9c8a5906603
flyingmouse <haoming.exe@gmail.com>
2018-03-08 16:28:38 +0100

    Qualification round

--- hashcode/2018/sol.cpp
+++ hashcode/2018/sol.cpp
@@ -22,186 +22,131 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 
-const int inf = 1e9;
-const int maxn = 1e5 + 5;
-int height, width, C, N, bonus, T, total;
-int used[maxn];
+struct Point {
+    int x, y;
+    Point(int x, int y): x(x), y(y) {}
+
+    int to(const Point& b) {
+        return abs(x - b.x) + abs(y - b.y);
+    }
+};
+
+ostream& operator << (ostream& out, const Point& point) {
+    out << "(" << point.x << ", " << point.y << ")";
+    return out;
+}
 
 struct Ride {
-    int sx, sy, tx, ty, start, end, index;
-
-    Ride(int a, int b, int x, int y, int s, int f, int index): sx(a), sy(b), tx(x), ty(y), start(s), end(f), index(index) {}
+    Point source, target;
+    int start, end, latestStart, index, carIndex;
+    Ride(int a, int b, int x, int y, int s, int f, int index): source(Point(a, b)), target(Point(x, y)), start(s), end(f), index(index) {
+        latestStart = end - distance();
+        carIndex = -1;
+    }
 
     int distance() {
-        return abs(tx - sx) + abs(ty - sy);
-    }
-
-    bool operator < (const Ride& b) {
-        if (start == b.start) return end < b.end;
-        return start < b.start;
+        return source.to(target);
     }
 };
+
+ostream& operator << (ostream& out, const Ride& ride) {
+    out << "ride #" << ride.index << ", from " << ride.source << " to " << ride.target;
+    return out;
+}
 
 struct Car {
-    int now, x, y, point;
-    vector<int> rides;
-    Car(): now(0), x(0), y(0), point(0) {}
-
-    int take(Ride& ride) {
-        assert(!used[ride.index]);
-        used[ride.index] = true;
-        rides.push_back(ride.index);
-        int s = score(ride);
-        total += s, point += s;
-
-        now += toStartPoint(ride);
-        now = max(now, ride.start);
-        now += ride.distance();
-        x = ride.tx, y = ride.ty;
-
-        return x;
-    }
-
-    int fakeTake(Ride& ride) {
-        rides.push_back(ride.index);
-        int s = score(ride);
-        point += s;
-
-        now += toStartPoint(ride);
-        now = max(now, ride.start);
-        now += ride.distance();
-        x = ride.tx, y = ride.ty;
-
-        return x;
-    }
-
-    int score(Ride& ride) {
-        int start = ride.start, end = ride.end;
-        int current = now + toStartPoint(ride);
-        current = max(current, start);
-        int score = 0;
-        if (current == start) score += bonus;
-        int d = ride.distance();
-        score += d, current += d;
-        if (current <= end && current <= T + 1) return score;
-        return 0;
-    }
-
-    int toStartPoint(Ride& ride) {
-        return abs(x - ride.sx) + abs(y - ride.sy);
-    }
-
-    int evaluate(Ride& ride) {
-        int d = toStartPoint(ride);
-        int arrivingTime = now + d;
-        return d + max(ride.start - arrivingTime, 0);
-    }
-
-    void print() {
-        printf("%d ", SZ(rides));
-        for (auto& ride : rides)
-            printf("%d ", ride);
-        printf("\n");
-    }
-
-    void clear() {
-        for (int x : rides) used[x] = false;
-        rides.clear();
-        total -= point;
-        point = 0;
-        now = 0;
-        x = 0, y = 0;
+    int freeTime;
+    Point position;
+    bool occupied;
+    Car(): position(Point(0, 0)) {
+        occupied = false;
+        freeTime = 0;
     }
 };
 
-vector<Ride> rides, profitRides;
+struct Event {
+    const static int FINISH = 1;
+    const static int START = 2;
+    const static int LATEST = 3; // now or never
+
+    int type, rideIndex, time;
+    Event(int type, int rideIndex, int time): type(type), rideIndex(rideIndex), time(time) {
+        assert(type > 0 && type < 4);
+    }
+};
+
+bool operator > (const Event& a, const Event& b) {
+    if (a.time != b.time) return a.time > b.time;
+    return a.type > b.type;
+}
+
+const int inf = 1e9;
+const int maxn = 1e5 + 5;
+int height, width, C, N, bonus, T, score, rds;
+int used[maxn];
+vector<int> ans[maxn];
+
+vector<Ride> rides;
 vector<Car> cars;
+priority_queue<Event, vector<Event>, greater<Event> > events;
+priority_queue<int, vector<int>, greater<int> > finishes;
 
-void getProfitableRides() {
-    // insure to get most profitable rides
-    Car car = Car();
-    vector<pii> ranking;
-    for (auto& ride : rides) {
-        ranking.push_back(MP(car.score(ride), ride.index));
-    }
-    sort(ALL(ranking));
-    reverse(ALL(ranking));
-    for (auto& p : ranking) {
-        profitRides.push_back(rides[p.second]);
-    }
+void finish(Event& event) {
+    Ride ride = rides[event.rideIndex];
+    int carIndex = ride.carIndex;
+    assert(carIndex >= 0);
+    assert(cars[carIndex].occupied);
+    cars[carIndex].occupied = false;
+    cars[carIndex].position = ride.target;
+    cars[carIndex].freeTime = event.time;
+    ans[carIndex].push_back(ride.index);
+    finishes.pop();
+    score += ride.distance();
 }
 
-void dispatchProfitableRides() {
-    int dispatched = 0;
-    for (auto& ride : profitRides) {
-        int best = 0, index = 0, minLoss = inf;
-        REP(i, C) {
-            Car car = cars[i];
-            int score = car.score(ride);
-            if (score > best || (score == best && car.evaluate(ride) < minLoss))
-                best = score, index = i, minLoss = min(minLoss, car.evaluate(ride));
+int findClosestCar(Ride& ride, int time) {
+    int best = -1, minArrive = inf;
+    REP(i, C) if (!cars[i].occupied) {
+        int arrive = cars[i].freeTime + cars[i].position.to(ride.source);
+        if (arrive > time) continue;
+        if (arrive < minArrive) minArrive = arrive, best = i;
+    }
+    return best;
+}
+
+void dispatch(Event& event) {
+    Ride ride = rides[event.rideIndex];
+    int carIndex = findClosestCar(ride, event.time);
+    if (carIndex == -1) {
+        int nextTime = event.time + 1; // no car can finish this event on time, postpone
+        if (SZ(finishes) == C) {
+            nextTime = finishes.top(); // to ensure there is at least one car available for the event to be created
         }
-        if (best > 0) {
-            dispatched++;
-            cars[index].take(ride);
+        if (nextTime < ride.latestStart) {
+            events.push(Event(Event::START, ride.index, nextTime));
         }
+        return;
     }
-    clog << "dispatched profitable rides: " << dispatched << " / " << N << endl;
+    assert(!cars[carIndex].occupied);
+    cars[carIndex].occupied = true;
+    rides[ride.index].carIndex = carIndex;
+    used[ride.index] = true;
+    int finishTime = cars[carIndex].freeTime + cars[carIndex].position.to(ride.source);
+    finishTime = max(finishTime, event.time);
+    finishTime += ride.distance();
+    finishes.push(finishTime);
+    events.push(Event(Event::FINISH, ride.index, finishTime));
+
+    if (event.time == ride.start) score += bonus;
 }
 
-int calculate(vector<int>& r) {
-    Car car = Car();
-    for (int x : r) {
-        car.fakeTake(rides[x]);
+void print() {
+    REP(i, C) {
+        printf("%d", SZ(ans[i]));
+        rds += SZ(ans[i]);
+        for (int x : ans[i]) printf(" %d", x);
+        printf("\n");
     }
-    return car.point;
-}
-
-void insertAt(vector<int>& r, int pos, int element) {
-    r.insert(r.begin() + pos, element);
-}
-
-void removeAt(vector<int>& r, int pos) {
-    r.erase(r.begin() + pos);
-}
-
-void localSearch() {
-    int cnt = 0;
-    vector<int> solution;
-    for (auto& car : cars) {
-        int before = car.point;
-        solution.clear();
-        for (int x : car.rides) solution.push_back(x);
-        bool changed = false;
-        // TBD
-        for (auto& ride : profitRides) if (!used[ride.index]) {
-            REP(j, SZ(solution) + 1) {
-                insertAt(solution, j, ride.index);
-                int current = calculate(solution);
-                if (current > before) {
-                    before = current;
-                    changed = true;
-                    cnt++;
-                    break;
-                }
-                removeAt(solution, j);
-            }
-        }
-        if (changed) {
-            // for (int x : car.rides) clog << x << " "; clog << endl;
-            // for (int x : solution) clog << x << " "; clog << endl;
-            car.clear();
-            for (int x : solution)
-                car.take(rides[x]);
-        }
-    }
-    clog << "inserted " << cnt << " rides" << endl;
-}
-
-void greedy() {
-    getProfitableRides();
-    dispatchProfitableRides();
-    localSearch();
 }
 
 int main(int argc, char* argv[]) {
@@ -219,21 +164,30 @@ int main(int argc, char* argv[]) {
     int a, b, x, y, s, f;
     REP(i, N) {
         cin >> a >> b >> x >> y >> s >> f;
-        rides.push_back(Ride(a, b, x, y, s, f, i));
+        Ride ride = Ride(a, b, x, y, s, f, i);
+        rides.push_back(ride);
+        int startTime = max(a + b, s);
+        if (startTime < ride.latestStart)
+            events.push(Event(Event::START, i, startTime));
+        events.push(Event(Event::LATEST, i, ride.latestStart));
     }
     REP(i, C) {
         cars.push_back(Car());
     }
 
-    greedy();
-
-    int rds = 0;
-    for (auto& car : cars) {
-        car.print();
-        rds += car.rides.size();
+    while (!events.empty()) {
+        Event event = events.top(); events.pop();
+        if (event.type == Event::FINISH) {
+            finish(event);
+            continue;
+        }
+        if (used[event.rideIndex]) continue;
+        dispatch(event);
     }
+    print();
+
     clog << "rides: " << rds  << " / " << N << endl;
-    clog << "score: " << total << endl;
+    clog << "score: " << score << endl;
 
     return 0;
 }
