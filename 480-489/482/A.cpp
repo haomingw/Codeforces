411f70b6ef9f60605143f68397674bba22a599c1
flyingmouse <haoming.exe@gmail.com>
2018-06-01 10:57:14 +0200

    Pizza, Pizza, Pizza!!!

---
+++ 480-489/482/A.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <set>
+#include <queue>
+#include <utility>
+#include <algorithm>
+#include <map>
+#include <cassert>
+using namespace std;
+
+#define REPP(i,a,b) for(int i=(a); i < (b); ++i)
+#define REP(i,a) for(int i = 0; i < (a); ++i)
+#define PER(i,a) for(int i = (a) - 1; i >= 0; --i)
+#define SZ(x) ((int)(x).size())
+#define ALL(x) (x).begin(), (x).end()
+#define MP make_pair
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+ll n;
+
+int main() {
+    cin >> n;
+    n += 1;
+    if (n == 1) cout << 0 << endl;
+    else if (n & 1) cout << n << endl;
+    else cout << n / 2 << endl;
+    return 0;
+}
