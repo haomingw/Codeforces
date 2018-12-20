cbb4618306c13960de066205985516128fddb5e8
haoming <haoming.exe@gmail.com>
2018-12-20 11:17:46 +0100

    Teams Forming
    http://codeforces.com/contest/1092/problem/B

---
+++ 520-529/527/B.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <set>
+#include <queue>
+#include <utility>
+#include <algorithm>
+#include <map>
+#include <cassert>
+using namespace std;
+
+#define REPP(i,a,b) for(int i=(a); i < (b); ++i)
+#define REP(i,a) for(int i = 0; i < (a); ++i)
+#define PER(i,a) for(int i = (a) - 1; i >= 0; --i)
+#define SZ(x) ((int)(x).size())
+#define ALL(x) (x).begin(), (x).end()
+#define MP make_pair
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+const int maxn = 105;
+int a[maxn];
+
+int main() {
+    int n;
+    cin >> n;
+    REP(i, n) cin >> a[i];
+    sort(a, a + n);
+    int ans = 0;
+    for (int i = 0; i < n - 1; i += 2) {
+        ans += a[i + 1] - a[i];
+    }
+    cout << ans << endl;
+    return 0;
+}
