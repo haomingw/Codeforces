ff03a11de838e455e837a4dcbbcbd32d0f5f1640
haoming <haoming.exe@gmail.com>
2018-12-30 01:34:32 +0100

    New Year and the Christmas Ornament
    http://codeforces.com/contest/1091/problem/A

--- annual/goodbye2018/A.cpp
+++ annual/goodbye2018/A.cpp
@@ -11,6 +11,7 @@
 #include <map>
 #include <cassert>
 using namespace std;
+
 #define REPP(i,a,b) for(int i = (a); i < (b); ++i)
 #define REP(i,a) for(int i = 0; i < (a); ++i)
 #define PER(i,a) for(int i = (a) - 1; i >= 0; --i)
@@ -21,3 +22,10 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    int ans = 3 * min(b, min(a + 1, c - 1));
+    cout << ans << endl;
+    return 0;
+}
