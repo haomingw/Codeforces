01e8d3ddd00792db016f2d4c1935a2abf40f3eb4
flyingmouse <haoming.exe@gmail.com>
2019-02-23 13:50:05 +0100

    Sea Battle

---
+++ 540-549/541/A.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <set>
+#include <queue>
+#include <utility>
+#include <algorithm>
+#include <map>
+#include <cassert>
+using namespace std;
+#define REPP(i,a,b) for(int i = (a); i < (b); ++i)
+#define REP(i,a) for(int i = 0; i < (a); ++i)
+#define PER(i,a) for(int i = (a) - 1; i >= 0; --i)
+#define SZ(x) ((int)(x).size())
+#define ALL(x) (x).begin(), (x).end()
+#define MP make_pair
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+int main() {
+    int w1, h1, w2, h2;
+    cin >> w1 >> h1 >> w2 >> h2;
+    int ans = (w1 + h1 + 4) * 2 - 4;
+    ans += 2 * h2;
+    cout << ans << endl;
+    return 0;
+}
