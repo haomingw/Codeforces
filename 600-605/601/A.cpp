dcf0181db52906a5b597ce3efb12595bcce0f047
flyingmouse <haoming.exe@gmail.com>
2019-11-20 10:27:43 +0100

    Changing Volume

---
+++ 600-605/601/A.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <set>
+#include <queue>
+#include <utility>
+#include <algorithm>
+#include <map>
+#include <cassert>
+using namespace std;
+#define REPP(i,a,b) for(int i = (a); i < (b); ++i)
+#define REP(i,a) for(int i = 0; i < (a); ++i)
+#define PER(i,a) for(int i = (a) - 1; i >= 0; --i)
+#define SZ(x) ((int)(x).size())
+#define ALL(x) (x).begin(), (x).end()
+#define MP make_pair
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+void solve() {
+  int a, b;
+  scanf("%d%d", &a, &b);
+  int d = abs(a - b);
+  int ans = 0;
+  ans += d / 5;
+  d %= 5;
+  ans += d / 2;
+  d %= 2;
+  ans += d;
+  printf("%d\n", ans);
+}
+
+int main() {
+  int t;
+  scanf("%d", &t);
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
