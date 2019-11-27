d381afaccd5ec5e8de57c32bcb02811366442af9
flyingmouse <haoming.exe@gmail.com>
2019-11-27 16:11:40 +0100

    Sweet Problem

---
+++ 600-605/603/A.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <set>
+#include <queue>
+#include <utility>
+#include <algorithm>
+#include <map>
+#include <bitset>
+#include <cassert>
+using namespace std;
+
+#define REPP(i,a,b) for(int i = (a); i < (b); ++i)
+#define REP(i,a) for(int i = 0; i < (a); ++i)
+#define PER(i,a) for(int i = (a) - 1; i >= 0; --i)
+#define SZ(x) ((int)(x).size())
+#define ALL(x) (x).begin(), (x).end()
+#define MP make_pair
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+void solve() {
+  int a[3];
+  scanf("%d%d%d", a, a + 1, a + 2);
+  sort(a, a + 3);
+  int ans = a[0];
+  int diff = a[2] - a[1];
+  if (diff >= a[0]) ans += a[1];
+  else {
+    ans += a[1] - (a[0] - diff + 1) / 2;
+  }
+  printf("%d\n", ans);
+}
+
+int main() {
+  int t;
+  scanf("%d", &t);
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
